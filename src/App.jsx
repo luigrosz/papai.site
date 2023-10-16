002eb3e8a1143534b815710420d1975eda0b3d11
Lui Grosz <luigrosz08@gmail.com>
2023-10-16 16:24:50 -0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>LEAGUE OF PAPAI</h1>
+      <h3>(under construction)</h3>
+    </>
+  )
+}
+
+export default App
